b0865006c678e3c3dfc3a62bd2d66fef5da603a2
Atanas Beychev <abeychev@dd-fs.co.uk>
2019-10-23 16:10:34 +0300

    Get post data & show json result of file metadate,  add form and additional styling

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var multer = require('multer');
 // require and use "multer"...
 
 var app = express();
-
+var upload = multer({dest: __dirname + '/uploads'})
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -20,6 +20,14 @@ app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
 });
 
+
+app.post('/upload', upload.single('file'), (req, res, next)=>{
+  // res.json(req.file); - all file information
+
+  res.json({"size": req.file.size});
+});
+
 app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
 });
+
